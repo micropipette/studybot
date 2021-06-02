d2ae99efb084d39915655c6e3e1f67ef4460dc9c
Mingde Yin <mingdeyin@gmail.com>
2021-06-01 21:45:48 -0400

    Added support for top.gg API
    Now we will show our bot count!

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,6 +1,6 @@
 from .testing import Testing
 from .quiz import Quiz
-from .settings import Settings
+from .settings import Settings, Topgg
 
 # The cogs to be loaded by the bot at runtime
-cogs = [Testing, Quiz, Settings]
+cogs = [Testing, Quiz, Settings, Topgg]
